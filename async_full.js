2b6cd8a08233a538b59885082c2c27b86f846740
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-02-20 13:07:40 -0500

    Update async_full.js

--- async_full.js
+++ async_full.js
@@ -18,57 +18,76 @@ console.log(pages);
 var containArr = [];
 var contactArr = [];
 
-async function getCompanyIds(p, type){
-	var res = await fetch("https://www.thalamus.co/"+type+"?page=" + (p + 1), {
-        "credentials": "include",
-        "headers": {
-          "accept": "*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript",
-          "accept-language": "en-US,en;q=0.9",
-          "x-csrf-token": "zwgDu/HrFaodOcW9JvhBAMvOOSGdYFp7GwKwipE+0uTQ6lKoVKDyzCmspcI9KP9eu95AX8nU1Sd3ABJvtm1C5g==",
-          "x-requested-with": "XMLHttpRequest"
-        },
-        "referrer": "https://www.thalamus.co/ad_partners",
-        "referrerPolicy": "no-referrer-when-downgrade",
-        "body": null,
-        "method": "GET",
-        "mode": "cors"
-      });
-	var text = await res.text();
-	var regXgroups = /(?<=ad_partner_)(.+?)\\'.+?href=\\'\/ad_partners\/(.+?)\\.+?item__title\\'>\\n(.+?)\\n/;
-	var ids = text.match( /ad_partner_.+?\\'.+?href=\\'\/ad_partners\/.+?\\.+?item__title\\'>\\n.+?\\n/g);
-	if(ids){
-	ids.forEach(async itm => {
-		var rx = regXgroups.exec(itm);
-		var inArr = containArr.some(elm=> elm == rx[1]);
-		if(inArr){
-			console.log(rx[2] + ' already in array'); 
+async function getCompanyIds(p, type) {
+  var res = await fetch("https://www.thalamus.co/" + type + "?page=" + (p + 1), {
+    "credentials": "include",
+    "headers": {
+      "accept": "*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript",
+      "accept-language": "en-US,en;q=0.9",
+      "x-csrf-token": "zwgDu/HrFaodOcW9JvhBAMvOOSGdYFp7GwKwipE+0uTQ6lKoVKDyzCmspcI9KP9eu95AX8nU1Sd3ABJvtm1C5g==",
+      "x-requested-with": "XMLHttpRequest"
+    },
+    "referrer": "https://www.thalamus.co/ad_partners",
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": null,
+    "method": "GET",
+    "mode": "cors"
+  });
+  var text = await res.text();
+  if (text) {
+    var regXgroups = /(?<=ad_partner_)(.+?)\\'.+?href=\\'\/ad_partners\/(.+?)\\.+?item__title\\'>\\n(.+?)\\n/;
+    var ids = text.match(/ad_partner_.+?\\'.+?href=\\'\/ad_partners\/.+?\\.+?item__title\\'>\\n.+?\\n/g);
+    if (ids) {
+      ids.forEach(async itm => {
+        var rx = regXgroups.exec(itm);
+        var inArr = containArr.some(elm => elm == rx[1]);
+        if (inArr) {
+          console.log(rx[2] + ' already in array');
         } else {
-			containArr.push([rx[1], rx[2], rx[3]]);
-			await delay(1481 + Math.round(Math.random() * 100));
-			getContactsByCoId([rx[1], rx[2], rx[3]]);
+          containArr.push([rx[1], rx[2], rx[3]]);
+          await delay(1481 + Math.round(Math.random() * 100));
+          getContactsByCoId([rx[1], rx[2], rx[3]]);
         }
-	});
-	}
+      });
+    }
+  } else {
+    console.log('page failed')
+  }
 }
 
-async function looper(str){
-	for(i=0; i<pages; i++){
-		await delay(11781 + Math.round(Math.random() * 100));
-		getCompanyIds(i,str);
-		console.log(i);
+async function getContactsByCoId(arr) {
+  var id = arr[0];
+  var res = await fetch("https://www.thalamus.co/contacts?type=1&type_id=" + id, {
+    "credentials": "include",
+    "headers": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "max-age=0",
+      "if-none-match": "W/\"2a2007269f957d6978bced59cd0827e9\"",
+      "upgrade-insecure-requests": "1"
+    },
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": null,
+    "method": "GET",
+    "mode": "cors"
+  });
+  var dat = await res.json();
+  if (dat) {
+    for (let i = 0; i < dat.length; i++) {
+      var csv = [(dat[i].id), xComma(dat[i].name), xComma(dat[i].title), (arr[2]), xComma(dat[i].number), xComma(dat[i].email), xComma(dat[i].location), xComma(dat[i].linkedin), xComma(dat[i].skype), (dat[i].account_id), (dat[i].parent_id), (dat[i].contact_type), (dat[i].contact_type_id), (dat[i].is_hidden), (dat[i].created_at), (dat[i].updated_at), (dat[i].resolved), (dat[i].submitter_id), 'https://www.thalamus.co/ad_partners/' + arr[1], id];
+      contactArr.push(csv);
     }
+  } else {
+    console.log('contact failed')
+  }
+}
+
+async function looper(str) {
+  for (let i = 0; i < pages; i++) {
+    await delay(8781 + Math.round(Math.random() * 100));
+    getCompanyIds(i, str);
+    console.log(i);
+  }
 }
 
 looper('ad_partners');
-
-
-async function getContactsByCoId(arr){
-	var id = arr[0];
-	var res = await fetch("https://www.thalamus.co/contacts?type=1&type_id="+id, {"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","cache-control":"max-age=0","if-none-match":"W/\"2a2007269f957d6978bced59cd0827e9\"","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"});
-	var dat = await res.json();
-	console.log(dat);
-	for(i = 0; i < dat.length; i++){
-		var csv = [(dat[i].id), xComma(dat[i].name), xComma(dat[i].title), (arr[2]), xComma(dat[i].number), xComma(dat[i].email), xComma(dat[i].location), xComma(dat[i].linkedin), xComma(dat[i].skype), (dat[i].account_id), (dat[i].parent_id), (dat[i].contact_type), (dat[i].contact_type_id), (dat[i].is_hidden), (dat[i].created_at), (dat[i].updated_at), (dat[i].resolved), (dat[i].submitter_id), 'https://www.thalamus.co/ad_partners/'+arr[1], id];
-		contactArr.push(csv);
-	}
-}
